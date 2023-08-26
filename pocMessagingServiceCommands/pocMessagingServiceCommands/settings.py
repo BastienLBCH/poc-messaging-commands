b653661e47eb8409d4cfcef1d6ffb9444f5bfd88
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-08-26 21:06:52 +0200

    refactor

--- pocMessagingServiceCommands/pocMessagingServiceCommands/settings.py
+++ pocMessagingServiceCommands/pocMessagingServiceCommands/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pocMessagingServiceCommands.middlewares.middleware.JWTAuthentificationMiddleware',
+    'pocMessagingServiceCommands.middlewares.authmiddleware.JWTAuthentificationMiddleware',
 ]
 
 ROOT_URLCONF = 'pocMessagingServiceCommands.urls'
